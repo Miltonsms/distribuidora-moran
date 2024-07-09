66647445ac17f096ae3e6d326be1ec27361d65ac
Miltonsms <milsmsramirezz@gmail.com>
2024-07-09 16:41:39 -0600

    envio de correo configurado dominio empresarial

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,20 +3,23 @@ import * as admin from 'firebase-admin';
 import * as nodemailer from 'nodemailer';
 
 admin.initializeApp();
-
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'mail.dismogt.com', // Cambia esto al host SMTP de tu proveedor
+  port: 26, // Cambia esto al puerto adecuado (usualmente 587 o 465)
+  secure: false, // true para port 465, false para otros puertos
   auth: {
-    user: 'estuardosms@gmail.com',
-    pass: 'ale24427592sms'
+    user: 'notificacion@dismogt.com',
+    pass: 't6ulpg56i35e'
+  },
+  tls: {
+    rejectUnauthorized: false, // A veces es necesario para proveedores de SMTP personalizados
   }
 });
-
 export const sendEmail = functions.https.onCall(async (data, context) => {
   const { to, subject, text } = data;
 
   const mailOptions = {
-    from: 'estuardosms@gmail.com',
+    from: 'notificacion@dismogt.com',
     to: to,
     subject: subject,
     text: text
